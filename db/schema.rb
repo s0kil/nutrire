c15e59a0ff645d472a86038e80dfe306c3cce465
Daniel Sokil <dtechtours@gmail.com>
2020-10-27 12:29:01 -0400

    Make Category Title Unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_225053) do
+ActiveRecord::Schema.define(version: 2020_10_27_162642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_10_26_225053) do
     t.decimal "priority"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_categories_on_title", unique: true
   end
 
   create_table "nutrients", force: :cascade do |t|
